dbd215ee57dcf9071afb7af8decafc37be8ab947
DenisHusainov <67679710+DenisHusainov@users.noreply.github.com>
2022-02-17 00:05:42 +0300

    Refactor PlayerController

--- RunnerCountMaster/Assets/Scripts/PlayerController.cs
+++ RunnerCountMaster/Assets/Scripts/PlayerController.cs
@@ -1,28 +1,18 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
 public class PlayerController : MonoBehaviour, IBeginDragHandler, IDragHandler
 { 
     [SerializeField] private float _speed;
-
-    private Rigidbody _rigidbody;
-
-    private void Awake()
-    {
-        _rigidbody = GetComponent<Rigidbody>();
-    }
+    [SerializeField] private Transform _player;
 
     public void OnBeginDrag(PointerEventData eventData)
     {
+        Vector2 delta = eventData.delta;
+
         if (eventData.delta.x>0)
         {
-            _rigidbody.AddForce(Vector3.right * _speed, ForceMode.Force);
-        }
-        else
-        {
-            _rigidbody.AddForce(Vector3.left * _speed, ForceMode.Force);
+            _player.position += new Vector3(delta.x * _speed, 0, 0);
         }
     }
 
