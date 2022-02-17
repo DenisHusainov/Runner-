d81181a47ccc197afa799e08c7224087baebda37
DenisHusainov <67679710+DenisHusainov@users.noreply.github.com>
2022-02-17 15:15:41 +0300

    Add move forward to PlayerController

--- RunnerCountMaster/Assets/Scripts/PlayerController.cs
+++ RunnerCountMaster/Assets/Scripts/PlayerController.cs
@@ -1,10 +1,16 @@
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class PlayerController : MonoBehaviour, IBeginDragHandler, IDragHandler
+public class PlayerController : MonoBehaviour, IBeginDragHandler, IDragHandler, IMoveHandler
 { 
     [SerializeField] private float _speed;
     [SerializeField] private Transform _player;
+    [SerializeField] private Rigidbody _rb;
+
+    private void FixedUpdate()
+    {
+        //_rb.velocity = new Vector3(0,0,_speed);
+    }
 
     public void OnBeginDrag(PointerEventData eventData)
     {
@@ -14,6 +20,7 @@ public class PlayerController : MonoBehaviour, IBeginDragHandler, IDragHandler
         {
             if (eventData.delta.x > 0)
             {
+                //transform.Translate(Vector3.right);
                 _player.position += new Vector3(delta.x * _speed, 0, 0);
                 Debug.Log("Right");
             }
@@ -28,5 +35,10 @@ public class PlayerController : MonoBehaviour, IBeginDragHandler, IDragHandler
     public void OnDrag(PointerEventData eventData)
     {
 
+    }
+
+    public void OnMove(AxisEventData eventData)
+    {
+       
     }
 }
