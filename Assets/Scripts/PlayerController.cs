72ca9ddbacde72177a98129ca26b2f508c8fe83b
DenisHusainov <67679710+DenisHusainov@users.noreply.github.com>
2022-02-17 20:01:49 +0300

    Setup git ignor

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class PlayerController : MonoBehaviour, IBeginDragHandler, IDragHandler, IMoveHandler
+{ 
+    [SerializeField] private float _speed;
+    [SerializeField] private Transform _player;
+    [SerializeField] private Rigidbody _rb;
+
+    private void FixedUpdate()
+    {
+        //_rb.velocity = new Vector3(0,0,_speed);
+    }
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        Vector2 delta = eventData.delta;
+
+        if (Mathf.Abs(delta.x) > Mathf.Abs(delta.y))
+        {
+            if (eventData.delta.x > 0)
+            {
+                //transform.Translate(Vector3.right);
+                //_player.position += new Vector3(delta.x * _speed, 0, 0);
+                _rb.AddForce(NewVector(delta.x, _speed));
+                Debug.Log("Right");
+            }
+            else
+            {
+                Debug.Log("Left");
+            }
+        }
+        
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+
+    }
+
+    public void OnMove(AxisEventData eventData)
+    {
+       
+    }
+
+    private Vector3 NewVector(float _x, float _turnSpeed)
+    {
+        return new Vector3(_x * Time.deltaTime * _turnSpeed, 0, 0);
+    }
+}
